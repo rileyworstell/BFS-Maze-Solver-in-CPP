8c3d719ba416acfa779796062066693cd55b617c
Riley Worstell <rcwors02@gmail.com>
2022-12-27 16:17:27 -0500

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+### Maze Solver Using BFS
+In the main.cpp file you can modify the board as you like. Then run the file and it will return the path you need to take to solve the board.
+On the board the 0's are open spaces and the 1's are walls.
---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+// #include <algorithm>
+
+using namespace std;
+
+vector<string> bfs(vector<vector<int>> &board) {
+    unordered_map<string, string> prevs;
+    set<vector<int>> visited;
+
+    vector<string> path;
+    int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    queue<vector<int>> q;
+    q.push({0, 0, -1, -1}); // (row, col, prevRow, prevCol)
+    while (q.size() != 0) {
+      int row, col, prevRow, prevCol;
+      auto back = q.front();
+      row = back[0];
+      col = back[1];
+      prevRow = back[2];
+      prevCol = back[3];
+      // set the value to look back at history
+      prevs[to_string(row) + "," + to_string(col)] = to_string(prevRow) + "," + to_string(prevCol);
+      q.pop();
+      visited.insert({row, col});
+
+      // board is solved
+      if (board[back[0]][back[1]] == -2) {
+        queue<vector<int>> empty;
+        swap(q, empty);
+      }
+      else {
+        // loop through directions
+        for (int dir = 0; dir < (sizeof(dirs)/sizeof(dirs[0])); dir++) {
+          int newR = row + dirs[dir][0];
+          int newC = col + dirs[dir][1];
+          if (newR > -1 && newR < board.size() && newC > -1 && newC < board[0].size()) {
+            if (visited.find({newR, newC}) == visited.end() && board[newR][newC] != 1) {
+              q.push({newR, newC, row, col});
+            }
+          }
+        }
+      }
+
+    }
+    // build the path from the prevs map
+    path.push_back(to_string(board.size()-1) + "," + to_string(board[0].size()-1));
+    auto point = prevs[to_string(board.size()-1) + "," + to_string(board[0].size()-1)];
+    while (point != "0,0") {
+      path.push_back(point);
+      point = prevs[point];
+    }
+    path.push_back(point);
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+int main() {
+  /*
+  -1 is starting point
+  -2 is ending point
+  0's are free spaces
+  1's are walls
+  */
+  vector<vector<int>> board = {
+    {-1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {1, 1, 1, 1, 0, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, -2}
+  };
+
+  vector<string> answer = bfs(board);
+  cout << "Your path is: " << endl;
+  for (int i = 0; i < answer.size(); i++) {
+    cout << answer[i][0] << " - " << answer[i][2] << endl;
+  }
+  return 0;
+}
