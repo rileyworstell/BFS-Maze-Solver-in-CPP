faf3b8a1a281431ca3bca2be020984a7e54241f9
Riley Worstell <rcwors02@gmail.com>
2022-12-27 17:51:21 -0500

    removing using namespace std

--- main.cpp
+++ main.cpp
@@ -5,26 +5,24 @@
 #include <string>
 #include <unordered_map>
 
-using namespace std;
-
-void buildPath(unordered_map<string, string> &prevs, vector<vector<int>> &board, vector<string> &path) {
-    path.push_back(to_string(board.size()-1) + "," + to_string(board[0].size()-1));
-    auto point = prevs[to_string(board.size()-1) + "," + to_string(board[0].size()-1)];
+void buildPath(std::unordered_map<std::string, std::string> &prevs, std::vector<std::vector<int>> &board, std::vector<std::string> &path) {
+    path.push_back(std::to_string(board.size()-1) + "," + std::to_string(board[0].size()-1));
+    auto point = prevs[std::to_string(board.size()-1) + "," + std::to_string(board[0].size()-1)];
     while (point != "0,0") {
       path.push_back(point);
       point = prevs[point];
     }
     path.push_back(point);
-    reverse(path.begin(), path.end());
+    std::reverse(path.begin(), path.end());
 }
 
-vector<string> bfs(vector<vector<int>> &board) {
-    unordered_map<string, string> prevs;
-    set<vector<int>> visited;
+std::vector<std::string> bfs(std::vector<std::vector<int>> &board) {
+    std::unordered_map<std::string, std::string> prevs;
+    std::set<std::vector<int>> visited;
 
-    vector<string> path;
+    std::vector<std::string> path;
     int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    queue<vector<int>> q;
+    std::queue<std::vector<int>> q;
     q.push({0, 0, -1, -1}); // (row, col, prevRow, prevCol)
     while (q.size() != 0) {
       int row, col, prevRow, prevCol;
@@ -34,14 +32,14 @@ vector<string> bfs(vector<vector<int>> &board) {
       prevRow = back[2];
       prevCol = back[3];
       // set the value to look back at history
-      prevs[to_string(row) + "," + to_string(col)] = to_string(prevRow) + "," + to_string(prevCol);
+      prevs[std::to_string(row) + "," + std::to_string(col)] = std::to_string(prevRow) + "," + std::to_string(prevCol);
       q.pop();
       visited.insert({row, col});
 
       // board is solved
       if (board[back[0]][back[1]] == -2) {
-        queue<vector<int>> empty;
-        swap(q, empty);
+        std::queue<std::vector<int>> empty;
+        std::swap(q, empty);
       }
       else {
         // loop through directions
@@ -68,7 +66,7 @@ int main() {
   0's are free spaces
   1's are walls
   */
-  vector<vector<int>> board = {
+  std::vector<std::vector<int>> board = {
     {-1, 1, 0, 0, 0, 0, 0, 0},
     {0, 1, 0, 0, 0, 0, 0, 0},
     {0, 1, 0, 0, 0, 0, 0, 0},
@@ -79,10 +77,10 @@ int main() {
     {0, 0, 0, 0, 0, 0, 0, -2}
   };
 
-  vector<string> answer = bfs(board);
-  cout << "Your path is: " << endl;
+  std::vector<std::string> answer = bfs(board);
+  std::cout << "Your path is: " << std::endl;
   for (int i = 0; i < answer.size(); i++) {
-    cout << answer[i][0] << " - " << answer[i][2] << endl;
+    std::cout << answer[i][0] << " - " << answer[i][2] << std::endl;
   }
   return 0;
 }
